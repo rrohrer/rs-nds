cefa6533c2bed83b3d7f94b32494b64f593c8cdc
rrohrer <ryan.rohrer@gmail.com>
2020-01-26 12:47:44 -0800

    added videoSetMode and videoSetModeSub to the lib

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,18 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(not(feature = "use-bindgen"))]
 include!("bindings.rs");
+
+const REG_DSPCNT: *mut u32 = 0x04000000 as *mut u32;
+const REG_DSPCNT_SUB: *mut u32 = 0x04001000 as *mut u32;
+
+pub fn videoSetMode(mode: u32) {
+    unsafe {
+        *REG_DSPCNT = mode;
+    }
+}
+
+pub fn videoSetModeSub(mode: u32) {
+    unsafe {
+        *REG_DSPCNT_SUB = mode;
+    }
+}
